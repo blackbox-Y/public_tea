8513e509c67947549b22d227b93e5dec43fd52b1
blackbox-Y <vladimirlepa8@gmail.com>
2025-02-14 18:04:47 +0300

    Update TeaRepository.java

--- main/java/com/projects/teashop/tea/repository/TeaRepository.java
+++ main/java/com/projects/teashop/tea/repository/TeaRepository.java
@@ -16,23 +16,7 @@ public interface TeaRepository extends JpaRepository <Tea, Long> {
 
 	Slice <Tea> findBySort (Pageable pageable, tea_type sort);
 	
-	
-	
 	List <Tea> findByName (String name);
-	
-//	@Query("SELECT tea FROM Tea tea " +
-//            "WHERE (coalesce(:name, null) IS NULL OR tea.name IN :name) " +
-////            "AND (coalesce(:sort, null) IS NULL OR tea.sort IN :sort) " +
-//            "AND (coalesce(:region, null) IS NULL OR tea.region IN :region) " +
-//            "AND (coalesce(:year, null) IS NULL OR tea.year >= :year) " +
-//            "ORDER BY perfume.price ASC")
-//	Page <Tea> searchRequest (
-//			Pageable pageable,
-//			@Param("name") String name,
-////			@Param("sort") Set<tea_type> sort,
-//			@Param("region") String region,
-//			@Param("year") Short year
-//			);
 	
 	void deleteById (Long id);
 }
